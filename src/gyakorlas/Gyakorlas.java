0fbb9f8e87ad3d9bb425ae6ae54fbf22f3960999
Szöllősi Bálint (SZF_2023_2025) <szollosi.balint@diak.szamalk-szalezi.hu>
2024-01-23 10:33:45 +0100

    first commit

---
+++ src/gyakorlas/Gyakorlas.java
@@ -0,0 +1,102 @@
+
+package gyakorlas;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Gyakorlas {
+
+   static int[] listam=new int[10];
+   static int szorzat;
+    public static void main(String[] args) {
+      //  szavakhossza();
+         sorozat();
+         szorzata();
+         parosszamok();
+         listaosszefuz();
+        
+    }
+
+ //   private static void szavakhossza() {
+       // Scanner sc = new Scanner(System.in);
+       // System.out.println("Kérek egy szót!");
+       // String szo1=sc.nextLine();
+      //  System.out.println("Kérek egy másik szót!");
+     //   String szo2=sc.nextLine();
+     //   int kivonas;
+     //   if (szo1.length() < szo2.length()) {
+   //         System.out.println("a hosszabb szó: "+szo2);
+   //         kivonas=szo2.length()-szo1.length();
+   //     }else{
+   //         System.out.println("a hosszabb szó: "+szo1);
+   //         kivonas=szo1.length()-szo2.length();
+   //     }
+        
+  //      System.out.println("szavak hosszának különbsége: "+kivonas);
+ //   }
+//
+    private static void sorozat() {
+        
+        Random rnd = new Random();
+        int szam;
+        int minimum=2021210;
+        for (int i = 0; i < 10; i++) {
+            szam=rnd.nextInt(100,200);
+            listam[i]=szam;
+            if (listam[i]<minimum) {
+                minimum=listam[i];
+            }
+            System.out.println(szam);
+    
+        
+        }
+        System.out.println("legkisebb elem: "+minimum);
+    }
+
+    private static void szorzata() {
+        
+        for (int i = 0; i < listam.length; i++) {
+            szorzat=listam[0]*listam[i];
+        }
+        System.out.println(szorzat);
+    }
+
+    private static void parosszamok() {
+        int paros=0;
+        for (int i = 0; i < listam.length; i++) {
+            if (listam[i]%2==0) {
+                paros+=1;
+            }
+        
+        }
+        System.out.println("páros számok : "+paros);    
+    }
+
+    private static void listaosszefuz() {
+        int[]lista1=new int[10];
+        int[]lista2=new int[10];
+        int[]listaosszefuzott=new int[20];
+        Random vmi = new Random();
+        for (int i = 0; i < 10; i++) {
+            lista1[i]=vmi.nextInt(100);
+            lista2[i]=vmi.nextInt(100);
+        }
+        for (int j = 0; j < 10; j++) {
+             
+            listaosszefuzott[j]=lista1[j];   
+        }
+        for (int j = 10; j < 20; j++) {
+             
+            listaosszefuzott[j]=lista2[j-10];   
+        }
+        
+        for (int i = 0; i < listaosszefuzott.length; i++) {
+            System.out.println(listaosszefuzott[i]);
+        }
+       
+        }
+    }
+    
+
